27e97bcbc87fe65b271cc298bcaae63c61c6a0ec
farid <arid1995@mail.ru>
2016-06-27 19:02:28 +0300

    Presumably working collisions

--- Classes/Bullet.cpp
+++ Classes/Bullet.cpp
@@ -6,20 +6,43 @@
 
 USING_NS_CC;
 
-Bullet::Bullet(cocos2d::Layer *layer, float x, float y, float direction, float speed) {
+Bullet::Bullet(cocos2d::Layer* _layer, float x, float y, int _direction, float speed) {
     initWithSpriteFrameName("Kunai.png");
     setHitBox(getTextureRect());
     setSpeedX(speed);
     setPosition(x, y);
+    direction = _direction;
+
+    layer = _layer;
     layer->addChild(this);
+    ObjectList::getInstance()->addObject(this);
+
     scheduleUpdate();
     resume();
 }
 
 void Bullet::update(float delta) {
-    //FIXME::Hardcode!
+
     Rect hitbox = getHitBox();
+    setPosition(getPosition().x + direction * getSpeedX(), getPosition().y);
+    //FIXME::Hardcode!
     if ((hitbox.origin.x > 950 || hitbox.origin.y > 950) || (hitbox.origin.x < 50 || hitbox.origin.y < 50)) {
+        layer->removeChild(this);
         delete this;
+        return;
+    }
+
+    std::list<GameObject*> collided = getCollidedObjects(ObjectList::getInstance()->getList());
+
+    if (collided.size() != 0) {
+        layer->removeChild(this);
+        delete this;
+        return;
+    }
+
+    if (direction == -1) {
+        setFlippedX(true);
+    } else {
+        setFlippedX(false);
     }
 }
--- Classes/Bullet.h
+++ Classes/Bullet.h
@@ -3,10 +3,15 @@
 //
 #pragma once
 #include "DynamicObject.h"
+#include "ObjectList.h"
 
 class Bullet : public DynamicObject {
 public:
-    Bullet(cocos2d::Layer *layer, float x, float y, float direction, float speed = 4);
+    Bullet(cocos2d::Layer *layer, float x, float y, int direction, float speed = 4);
     ~Bullet() {};
     void update(float delta);
+
+private:
+    cocos2d::Layer *layer;
+    int direction;
 };
